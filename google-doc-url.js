78e5e93e40b5b3fbc6927b35c1aa66ddb648a43c
YUVAL SHFRIRI <yuval.shafriri@gmail.com>
2017-03-04 12:31:06 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Rutqxst05F6goJQ3Z5uh7QHSrea0Nz9B9j_mcnXObzI/edit#gid=0';
